29e4b1fabfa92d34f8b55f11c251817f4d7ce33b
IvanMarkov <vanymarkov1996@gmail.com>
2022-02-28 13:23:38 +0200

    fix numerical and table charts display

--- src/pptx-template.tsx
+++ src/pptx-template.tsx
@@ -1,23 +1,31 @@
 import { Presentation, Slide, Image, render } from "react-pptx";
 import React from "react";
+import sizeOf from "image-size";
 
-const PPTX = (files) => (
-  <Presentation>
-    {files.map((file, index) => (
-      <Slide key={index}>
-        <Image
-          src={{ kind: "data", data: file.data }}
-          style={{
-            x: "0%",
-            y: "0%",
-            w: "100%",
-            h: "100%",
-          }}
-        ></Image>
-      </Slide>
-    ))}
-  </Presentation>
-);
+const PPTX = (files) => {
+  if (!files) return null;
+  return (
+    <Presentation>
+      {files.map((file, index) => {
+        var img = Buffer.from(file.data.split(";base64,").pop(), "base64");
+        var dimensions = sizeOf(img);
+        return (
+          <Slide key={index}>
+            <Image
+              src={{ kind: "data", data: file.data }}
+              style={{
+                x: "0%",
+                y: "0%",
+                w: 10,
+                h: (10 * dimensions.height) / dimensions.width,
+              }}
+            ></Image>
+          </Slide>
+        );
+      })}
+    </Presentation>
+  );
+};
 
 export default async (files: any[]) => {
   return render(PPTX(files)).then((buffer: any) => {
